93e1954f5d0eb4b10f5b6a4e558e5cb73d478671
VincentJiang <jxs471494539@gmail.com>
2014-01-17 23:18:48 +0800

    add sending email foundtion to production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,16 @@ Jade::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'cac.com',
+    user_name:            'cacjade',
+    password:             'cacjade123',
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
 end
